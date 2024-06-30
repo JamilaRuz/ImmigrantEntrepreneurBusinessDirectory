8c59a1dfc3bc39832cf539b8761ec0ec2e4c9b09
Jamila <j.ruzimetova@gmail.com>
2024-06-29 21:31:39 -0400

    - Company entity was created in Firebase Store - Entreprener has related company Id.

---
+++ WomenBusinessDirectory/Views/CategoryManager.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryManager.swift
+//  WomenBusinessDirectory
+//
+//  Created by Jamila Ruzimetova on 6/29/24.
+//
+
+import Foundation
---
+++ WomenBusinessDirectory/Views/CompanyManager.swift
@@ -0,0 +1,8 @@
+//
+//  CompanyManager.swift
+//  WomenBusinessDirectory
+//
+//  Created by Jamila Ruzimetova on 6/29/24.
+//
+
+import Foundation
