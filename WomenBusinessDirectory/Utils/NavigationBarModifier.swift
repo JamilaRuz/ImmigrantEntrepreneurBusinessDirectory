44f244c07abd667c86791ff9b92fe649ddc35848
JamilaRuz <j.ruzimetova@gmail.com>
2024-10-16 17:05:03 -0400

    - Log in/Log out features

--- WomenBusinessDirectory/Utils/NavigationBarModifier.swift
+++ WomenBusinessDirectory/Utils/NavigationBarModifier.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct NavigationBarModifier: ViewModifier {
     @Binding var showSignInView: Bool
-    var isLoggedIn: Bool // Add a property to track login status
-    
+    @Binding var isLoggedIn: Bool // Use binding to update login status
+    @State private var showToast = false
+
     func body(content: Content) -> some View {
         content
             .toolbar {
@@ -22,19 +23,48 @@ struct NavigationBarModifier: ViewModifier {
                 }
                 
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: ProfileView(showSignInView: $showSignInView)) {
+                    Menu {
+                        if isLoggedIn {
+                            Button("Sign Out") {
+                                isLoggedIn = false
+                                showToast = true
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                    showToast = false
+                                }
+                            }
+                        } else {
+                            Button("Sign In") {
+                                showSignInView = true
+                            }
+                        }
+                    } label: {
                         Image(systemName: "person.crop.circle")
                             .imageScale(.large)
-                            .foregroundColor(isLoggedIn ? Color("pink1") : Color.gray) // Change color based on login status
+                            .foregroundColor(isLoggedIn ? Color("pink1") : Color.gray)
                     }
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
+            .overlay(
+                VStack {
+                    if showToast {
+                        Text("You have signed out")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                            .transition(.opacity)
+                            .zIndex(1)
+                    }
+                }
+                .animation(.easeInOut, value: showToast)
+                .padding(.top, 50) // Adjust based on your layout
+            )
     }
 }
 
 extension View {
-    func customNavigationBar(showSignInView: Binding<Bool>, isLoggedIn: Bool) -> some View {
+    func customNavigationBar(showSignInView: Binding<Bool>, isLoggedIn: Binding<Bool>) -> some View {
         self.modifier(NavigationBarModifier(showSignInView: showSignInView, isLoggedIn: isLoggedIn))
     }
 }
